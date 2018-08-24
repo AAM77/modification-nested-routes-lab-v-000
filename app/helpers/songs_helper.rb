e565d03fa3127c0ef10bc9c248dc77db136afcf9
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-24 19:33:20 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -4,7 +4,7 @@ module SongsHelper
     if song.artist
       hidden_field_tag "song[artist_id]", song.artist_id
     else
-      select_tag "song[artist_id]", 
+      select_tag "song[artist_id]", options_from_collection_fo_select
       
       
   end
