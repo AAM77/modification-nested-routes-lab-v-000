6b7a36ce2bab4ffcdc363aeb59b78a6e46bc541d
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-24 19:09:50 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -51,7 +51,8 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist
-        @song = Song.new(artist_id: params[:artist_id])
+        @song = Song.find_by(id: params[:id])
+        @song.artist_id = params[:artist_id])
       else
         redirect_to artists_path
       end
